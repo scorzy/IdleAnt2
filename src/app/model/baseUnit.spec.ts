65514975db6201ba671b50ac7da6795c29be8f8a
lorenzo <scorzy88@gmail.com>
2018-05-19 12:15:35 +0200

    test

--- src/app/model/baseUnit.spec.ts
+++ src/app/model/baseUnit.spec.ts
@@ -4,26 +4,27 @@ describe("BaseUnit", () => {
   it("should create an instance", () => {
     expect(new BaseUnit("id", "name", "desc")).toBeTruthy();
   });
-});
 
-describe("Save", () => {
-  const baseUnit = new BaseUnit("id", "name", "desc", new Decimal(10));
-  const other = new BaseUnit("id", "name", "desc");
+  describe("Save", () => {
+    const baseUnit = new BaseUnit("id", "name", "desc", new Decimal(10));
+    const other = new BaseUnit("id", "name", "desc");
 
-  other.restore(baseUnit.getSave());
+    other.restore(baseUnit.getSave());
 
-  it("Save works", () => {
-    expect(JSON.stringify(baseUnit)).toEqual(JSON.stringify(other));
+    it("Save works", () => {
+      expect(JSON.stringify(baseUnit)).toEqual(JSON.stringify(other));
+    });
   });
-});
 
-describe("Not load with different id", () => {
-  const baseUnit = new BaseUnit("old", "name", "desc", new Decimal(10));
-  const other = new BaseUnit("new", "name", "desc");
+  describe("Not load with different id", () => {
+    const baseUnit = new BaseUnit("old", "name", "desc", new Decimal(10));
+    const other = new BaseUnit("new", "name", "desc");
 
-  other.restore(baseUnit.getSave());
+    other.restore(baseUnit.getSave());
 
-  it("Not Loaded", () => {
-    expect(other.restore(baseUnit.getSave())).toBeFalsy();
+    it("Not Loaded", () => {
+      expect(other.restore(baseUnit.getSave())).toBeFalsy();
+    });
   });
+
 });
