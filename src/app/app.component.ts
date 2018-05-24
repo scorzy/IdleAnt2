967c207cca56706b2af3f8fe396168d936e0e514
lorenzo <scorzy88@gmail.com>
2018-05-24 23:02:15 +0200

    clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import * as Decimal from "break_infinity.js";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -7,4 +7,5 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "app";
+  num = new Decimal(2);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ClarityModule } from "@clr/angular";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule,ClarityModule],
   providers: [],
   bootstrap: [AppComponent]
 })
