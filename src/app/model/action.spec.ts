f57c76e415e5cab8840f10d04fec03a958defcf3
lorenzo <scorzy88@gmail.com>
2018-06-16 00:10:01 +0200

    tests

--- src/app/model/action.spec.ts
+++ src/app/model/action.spec.ts
@@ -1,46 +1,39 @@
 import { Action } from "./action";
-import { BaseUnit } from "./baseUnit";
 import { Price } from "./price";
 import { FullUnit } from "./full-unit";
+import { iterateListLike } from "@angular/core/src/change_detection/change_detection_util";
 
 describe("Action", () => {
+  let action: Action;
+  beforeEach(() => {
+    action = new Action("id", "name", "desc");
+  });
+
   it("should create an instance", () => {
     expect(new Action("id", "name", "desc")).toBeTruthy();
   });
-  describe("Save and Load", () => {
+  it("Save and Load", () => {
     const origin = new Action("id", "name", "desc");
     const other = new Action("id", "name", "desc");
 
     const save = origin.getSave();
     const result = other.restore(save);
 
-    it("should return true", () => {
-      expect(result).toBeTruthy();
-    });
-    it("should be equal", () => {
-      expect(JSON.stringify(origin)).toEqual(JSON.stringify(other));
-    });
-
     const different = new Action("id2", "name2", "desc2");
     const result2 = different.restore(save);
-
-    it("should return false", () => {
-      expect(result2).toBeFalsy();
-    });
 
     origin.complete = true;
     origin.done = true;
 
     const result3 = other.restore(origin.getSave());
-    it("should return true", () => {
-      expect(result3).toBeTruthy();
-    });
-    it("should be equal", () => {
-      expect(JSON.stringify(origin)).toEqual(JSON.stringify(other));
-    });
+
+    expect(result2).toBeFalsy();
+    expect(result).toBeTruthy();
+    expect(JSON.stringify(origin)).toEqual(JSON.stringify(other));
+    expect(result3).toBeTruthy();
+    expect(JSON.stringify(origin)).toEqual(JSON.stringify(other));
   });
-  describe("Reload 1", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload 1", () => {
     const unit1 = new FullUnit("u1", "name", "desc");
     const unit2 = new FullUnit("u2", "name", "desc");
     const unit3 = new FullUnit("u3", "name", "desc");
@@ -55,18 +48,11 @@ describe("Action", () => {
     unit3.quantity = new Decimal(0);
     action.reload();
 
-    it("not buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("max buy = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
-    it("not done", () => {
-      expect(action.done).toBeFalsy();
-    });
+    expect(action.canBuy).toBeFalsy();
+    expect(action.maxBuy.toNumber()).toBe(0);
+    expect(action.done).toBeFalsy();
   });
-  describe("Reload 2", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload 2", () => {
     const unit1 = new FullUnit("a1", "name", "desc");
     const unit2 = new FullUnit("a2", "name", "desc");
     const unit3 = new FullUnit("a3", "name", "desc");
@@ -81,15 +67,11 @@ describe("Action", () => {
     unit2.quantity = new Decimal(350);
     unit3.quantity = new Decimal(0);
     action.reload();
-    it("still not buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("max buy still = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
+
+    expect(action.canBuy).toBeFalsy();
+    expect(action.maxBuy.toNumber()).toBe(0);
   });
-  describe("Reload 3", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload 3", () => {
     const unit1 = new FullUnit("u1", "", "");
     const unit2 = new FullUnit("u2", "", "");
     const unit3 = new FullUnit("u3", "", "");
@@ -105,15 +87,10 @@ describe("Action", () => {
     unit3.quantity = new Decimal(1e10);
     action.reload();
 
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("max buy = 3", () => {
-      expect(action.maxBuy.toNumber()).toBe(3);
-    });
+    expect(action.canBuy).toBeTruthy();
+    expect(action.maxBuy.toNumber()).toBe(3);
   });
-  describe("Reload limited", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload limited", () => {
     const unit1 = new FullUnit("l1", "", "");
     const unit2 = new FullUnit("l2", "", "");
     const unit3 = new FullUnit("l3", "", "");
@@ -131,15 +108,10 @@ describe("Action", () => {
     action.limit = new Decimal(10);
     action.reload();
 
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("max buy = 3", () => {
-      expect(action.maxBuy.toNumber()).toBe(10);
-    });
+    expect(action.canBuy).toBeTruthy();
+    expect(action.maxBuy.toNumber()).toBe(10);
   });
-  describe("Reload limited 2", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload limited 2", () => {
     const unit1 = new FullUnit("l21", "", "");
     const unit2 = new FullUnit("l22", "", "");
     const unit3 = new FullUnit("l23", "", "");
@@ -158,15 +130,10 @@ describe("Action", () => {
     action.quantity = new Decimal(3);
     action.reload();
 
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("max buy = 7", () => {
-      expect(action.maxBuy.toNumber()).toBe(7);
-    });
+    expect(action.canBuy).toBeTruthy();
+    expect(action.maxBuy.toNumber()).toBe(7);
   });
-  describe("Reload limited 3", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload limited 3", () => {
     const unit1 = new FullUnit("u31", "", "");
     const unit2 = new FullUnit("u32", "", "");
     const unit3 = new FullUnit("u33", "", "");
@@ -185,15 +152,10 @@ describe("Action", () => {
     action.quantity = new Decimal(9);
     action.reload();
 
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("max buy = 1", () => {
-      expect(action.maxBuy.toNumber()).toBe(1);
-    });
+    expect(action.canBuy).toBeTruthy();
+    expect(action.maxBuy.toNumber()).toBe(1);
   });
-  describe("Reload limited 4", () => {
-    const action = new Action("id", "name", "desc");
+  it("Reload limited 4", () => {
     const unit1 = new FullUnit("u41", "", "");
     const unit2 = new FullUnit("u42", "", "");
     const unit3 = new FullUnit("u43", "", "");
@@ -212,15 +174,10 @@ describe("Action", () => {
     action.quantity = new Decimal(10);
     action.reload();
 
-    it("buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("max buy = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
+    expect(action.canBuy).toBeFalsy();
+    expect(action.maxBuy.toNumber()).toBe(0);
   });
-  describe("Buy 1", () => {
-    const action = new Action("id", "name", "desc");
+  it("Buy 1", () => {
     const unit1 = new FullUnit("b1", "", "");
     const unit2 = new FullUnit("b2", "", "");
     const unit3 = new FullUnit("b3", "", "");
@@ -238,30 +195,15 @@ describe("Action", () => {
 
     const ret = action.buy(new Decimal(2));
 
-    it("u1 quantity", () => {
-      expect(unit1.quantity.toNumber()).toBe(14);
-    });
-    it("return true", () => {
-      expect(ret).toBeTruthy();
-    });
-    it("quantity = 2", () => {
-      expect(action.quantity.toNumber()).toBe(2);
-    });
-    it("max buy = 1", () => {
-      expect(action.maxBuy.toNumber()).toBe(1);
-    });
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("done", () => {
-      expect(action.done).toBeTruthy();
-    });
-    it("completed", () => {
-      expect(action.complete).toBeFalsy();
-    });
+    expect(unit1.quantity.toNumber()).toBe(14);
+    expect(ret).toBeTruthy();
+    expect(action.quantity.toNumber()).toBe(2);
+    expect(action.maxBuy.toNumber()).toBe(1);
+    expect(action.canBuy).toBeTruthy();
+    expect(action.done).toBeTruthy();
+    expect(action.complete).toBeFalsy();
   });
-  describe("Buy 2", () => {
-    const action = new Action("id", "name", "desc");
+  it("Buy 2", () => {
     const unit1 = new FullUnit("b21", "", "");
     const unit2 = new FullUnit("b22", "", "");
     const unit3 = new FullUnit("b23", "", "");
@@ -278,21 +220,12 @@ describe("Action", () => {
     action.reload();
     const ret = action.buy(new Decimal(2));
 
-    it("return true", () => {
-      expect(ret).toBeFalsy();
-    });
-    it("buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("max buy = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
-    it("completed", () => {
-      expect(action.complete).toBeFalsy();
-    });
+    expect(ret).toBeFalsy();
+    expect(action.canBuy).toBeFalsy();
+    expect(action.maxBuy.toNumber()).toBe(0);
+    expect(action.complete).toBeFalsy();
   });
-  describe("Buy limited", () => {
-    const action = new Action("id", "name", "desc");
+  it("Buy limited", () => {
     const unit1 = new FullUnit("bl1", "", "");
 
     action.prices = [new Price(unit1, new Decimal(10), 1.1)];
@@ -302,21 +235,12 @@ describe("Action", () => {
     action.limit = new Decimal(10);
     action.buy(new Decimal(10));
 
-    it("max buy = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
-    it("buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("done", () => {
-      expect(action.done).toBeTruthy();
-    });
-    it("completed", () => {
-      expect(action.complete).toBeTruthy();
-    });
+    expect(action.maxBuy.toNumber()).toBe(0);
+    expect(action.canBuy).toBeFalsy();
+    expect(action.done).toBeTruthy();
+    expect(action.complete).toBeTruthy();
   });
-  describe("Buy last", () => {
-    const action = new Action("id", "name", "desc");
+  it("Buy last", () => {
     const unit1 = new FullUnit("ul1", "", "");
 
     action.prices = [new Price(unit1, new Decimal(10), 1.1)];
@@ -327,24 +251,13 @@ describe("Action", () => {
     action.quantity = new Decimal(9);
     const ret = action.buy(new Decimal(1));
 
-    it("bought", () => {
-      expect(ret).toBeTruthy();
-    });
-    it("buyable", () => {
-      expect(action.canBuy).toBeFalsy();
-    });
-    it("max buy = 0", () => {
-      expect(action.maxBuy.toNumber()).toBe(0);
-    });
-    it("done", () => {
-      expect(action.done).toBeTruthy();
-    });
-    it("completed", () => {
-      expect(action.complete).toBeTruthy();
-    });
+    expect(ret).toBeTruthy();
+    expect(action.canBuy).toBeFalsy();
+    expect(action.maxBuy.toNumber()).toBe(0);
+    expect(action.done).toBeTruthy();
+    expect(action.complete).toBeTruthy();
   });
-  describe("Buy all exepct one", () => {
-    const action = new Action("id", "name", "desc");
+  it("Buy all exepct one", () => {
     const unit1 = new FullUnit("qwe", "", "");
 
     action.prices = [new Price(unit1, new Decimal(10), 1.1)];
@@ -355,20 +268,10 @@ describe("Action", () => {
     action.quantity = new Decimal(8);
     const ret = action.buy(new Decimal(1));
 
-    it("bought", () => {
-      expect(ret).toBeTruthy();
-    });
-    it("buyable", () => {
-      expect(action.canBuy).toBeTruthy();
-    });
-    it("max buy = 1", () => {
-      expect(action.maxBuy.toNumber()).toBe(1);
-    });
-    it("done", () => {
-      expect(action.done).toBeTruthy();
-    });
-    it("not completed", () => {
-      expect(action.complete).toBeFalsy();
-    });
+    expect(ret).toBeTruthy();
+    expect(action.canBuy).toBeTruthy();
+    expect(action.maxBuy.toNumber()).toBe(1);
+    expect(action.done).toBeTruthy();
+    expect(action.complete).toBeFalsy();
   });
 });
--- src/app/model/baseUnit.spec.ts
+++ src/app/model/baseUnit.spec.ts
@@ -1,30 +1,25 @@
 import { BaseUnit } from "./baseUnit";
-import { Action } from "./action";
 
 describe("BaseUnit", () => {
   it("should create an instance", () => {
     expect(new BaseUnit("id", "name", "desc")).toBeTruthy();
   });
 
-  describe("Save", () => {
+  it("Save", () => {
     const baseUnit = new BaseUnit("id", "name", "desc", new Decimal(10));
     const other = new BaseUnit("id", "name", "desc");
 
     other.restore(baseUnit.getSave());
 
-    it("Save works", () => {
-      expect(JSON.stringify(baseUnit)).toEqual(JSON.stringify(other));
-    });
+    expect(JSON.stringify(baseUnit)).toEqual(JSON.stringify(other));
   });
 
-  describe("Not load with different id", () => {
+  it("Not load with different id", () => {
     const baseUnit = new BaseUnit("old", "name", "desc", new Decimal(10));
     const other = new BaseUnit("new", "name", "desc");
 
     other.restore(baseUnit.getSave());
 
-    it("Not Loaded", () => {
-      expect(other.restore(baseUnit.getSave())).toBeFalsy();
-    });
+    expect(other.restore(baseUnit.getSave())).toBeFalsy();
   });
 });
