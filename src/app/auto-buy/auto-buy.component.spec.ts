23b8ad2e63b31e6af36b7e6671444c0068c44a62
lorenzo <scorzy88@gmail.com>
2018-07-15 17:38:37 +0200

    tests

--- src/app/auto-buy/auto-buy.component.spec.ts
+++ src/app/auto-buy/auto-buy.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { AutoBuyComponent } from "./auto-buy.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ClarityModule } from "@clr/angular";
+import { ToastrModule } from "ngx-toastr";
+import { FormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormatPipe } from "../format.pipe";
+import { EndInPipe } from "../end-in.pipe";
+import { AutoBuy } from "../model/autoBuy/auto-buy";
+import { Action } from "../model/action";
 
 describe("AutoBuyComponent", () => {
   let component: AutoBuyComponent;
@@ -8,13 +18,22 @@ describe("AutoBuyComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AutoBuyComponent]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        ClarityModule.forRoot(),
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [AutoBuyComponent, FormatPipe, EndInPipe]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AutoBuyComponent);
     component = fixture.componentInstance;
+    component.autoBuy = new AutoBuy(new Action("", "", "", []), []);
     fixture.detectChanges();
   });
 
--- src/app/unit-auto-buy/unit-auto-buy.component.spec.ts
+++ src/app/unit-auto-buy/unit-auto-buy.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { UnitAutoBuyComponent } from "./unit-auto-buy.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ClarityModule } from "@clr/angular";
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from "@angular/forms";
+import { FormatPipe } from "../format.pipe";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { EndInPipe } from "../end-in.pipe";
+import { MainService } from "../main.service";
+import { SliderModule } from "primeng/slider";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 describe("UnitAutoBuyComponent", () => {
   let component: UnitAutoBuyComponent;
@@ -8,7 +17,15 @@ describe("UnitAutoBuyComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UnitAutoBuyComponent]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        ClarityModule.forRoot(),
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [UnitAutoBuyComponent, FormatPipe, EndInPipe]
     }).compileComponents();
   }));
 
