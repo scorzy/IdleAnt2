df28206cb945748d84c39aa133b7c777880e5ed8
Scorzy <scorzy88@gmail.com>
2018-12-24 16:31:34 +0100

    break_infinity.js types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -7,46 +7,46 @@
 //   export default value;
 // }
 
-declare class Decimal {
-  static min: (any, any2) => Decimal;
-  static max: (any, any2) => Decimal;
-  static pow: (any, any2) => Decimal;
-  static sumGeometricSeries: (any, any2, any3, any4) => Decimal;
-  static affordGeometricSeries: (
-    resourcesAvailable,
-    priceStart,
-    priceRatio,
-    currentOwned
-  ) => Decimal;
-  static ln: (any) => number;
-  static log10: (any) => number;
-  static sqrt: (any) => Decimal;
-  static cbrt: (any) => Decimal;
+// declare class Decimal {
+//   static min: (any, any2) => Decimal;
+//   static max: (any, any2) => Decimal;
+//   static pow: (any, any2) => Decimal;
+//   static sumGeometricSeries: (any, any2, any3, any4) => Decimal;
+//   static affordGeometricSeries: (
+//     resourcesAvailable,
+//     priceStart,
+//     priceRatio,
+//     currentOwned
+//   ) => Decimal;
+//   static ln: (any) => number;
+//   static log10: (any) => number;
+//   static sqrt: (any) => Decimal;
+//   static cbrt: (any) => Decimal;
 
-  eq: (any) => boolean;
-  gte: (any) => boolean;
-  gt: (any) => boolean;
-  lte: (any) => boolean;
-  lt: (any) => boolean;
+//   eq: (any) => boolean;
+//   gte: (any) => boolean;
+//   gt: (any) => boolean;
+//   lte: (any) => boolean;
+//   lt: (any) => boolean;
 
-  times: (any) => Decimal;
-  div: (any) => Decimal;
-  plus: (any) => Decimal;
-  min: (any) => Decimal;
+//   times: (any) => Decimal;
+//   div: (any) => Decimal;
+//   plus: (any) => Decimal;
+//   min: (any) => Decimal;
 
-  minus: (any) => Decimal;
-  max: (any) => Decimal;
-  pow: (any) => Decimal;
-  cmp: (any) => number;
-  ln: () => number;
-  log10: () => number;
-  floor: () => Decimal;
-  ceil: () => Decimal;
-  abs: () => Decimal;
-  sqrt: () => Decimal;
-  log: (base) => number;
-  cbrt: () => Decimal;
+//   minus: (any) => Decimal;
+//   max: (any) => Decimal;
+//   pow: (any) => Decimal;
+//   cmp: (any) => number;
+//   ln: () => number;
+//   log10: () => number;
+//   floor: () => Decimal;
+//   ceil: () => Decimal;
+//   abs: () => Decimal;
+//   sqrt: () => Decimal;
+//   log: (base) => number;
+//   cbrt: () => Decimal;
 
-  toNumber: () => number;
-  constructor(any);
-}
+//   toNumber: () => number;
+//   constructor(any);
+// }
